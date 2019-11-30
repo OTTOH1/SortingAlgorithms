94236a4fe51724dc969eb1814ebfa78bd648b472
Anton Fomenko <vafon2@gmail.com>
2019-11-30 09:41:44 +0200

    Updates

---
+++ precache-manifest.47161cfa5bf45c84121ef2f87e266709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f5210f88034398aed0e11c8e6976f6",
+    "url": "/SortingAlgorithms/index.html"
+  },
+  {
+    "revision": "9670f804391c661b5881",
+    "url": "/SortingAlgorithms/static/css/main.b97c5d97.chunk.css"
+  },
+  {
+    "revision": "ae4e3fa89f350a97ec16",
+    "url": "/SortingAlgorithms/static/js/2.6d8871a7.chunk.js"
+  },
+  {
+    "revision": "9670f804391c661b5881",
+    "url": "/SortingAlgorithms/static/js/main.bf767e20.chunk.js"
+  },
+  {
+    "revision": "8c722da1f11d934dcc72",
+    "url": "/SortingAlgorithms/static/js/runtime-main.232b1fdc.js"
+  }
+]);
